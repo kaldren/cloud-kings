2d380b3a6a42383c2b6a17f2ab194ea86ba5ced4
Kaloyan Drenski <drenski666@gmail.com>
2021-11-09 20:38:02 +0200

    Added search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="app">
         <h1>Cloud Kings</h1>
+        <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchBar() {
+    return (
+        <div id="searchBar">
+            Search bar
+        </div>
+    )
+}
+
+export default SearchBar
