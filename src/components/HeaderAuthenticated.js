3443c5916b3ab28a57a6bf35d70b132bffaac4dc
Kaloyan Drenski <drenski666@gmail.com>
2021-11-20 11:43:48 +0200

    Added logged in icons

--- src/components/HeaderAuthenticated.js
+++ src/components/HeaderAuthenticated.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
+
+// Icons
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ForumIcon from '@mui/icons-material/Forum';
 
 // Custom
 import {signOut} from '../auth'
@@ -14,9 +20,9 @@ function HeaderAuthenticated({user}) {
             {/* <Button title='Messages' route='' color='#fff' />
             <Button title={user.displayName} route='' color='#fff' Icon={PersonIcon} /> */}
             <DropdownButton id="dropdown-basic-button" title={user.displayName}>
-                <Dropdown.Item href="#/action-2">Messages</Dropdown.Item>
-                <Dropdown.Item href="#/action-1">Settings</Dropdown.Item>
-                <Link to="/" onClick={signOut} class="dropdown-item"> Logout </Link>
+                <Dropdown.Item href="#/action-2"><ForumIcon /> Messages</Dropdown.Item>
+                <Dropdown.Item href="#/action-1"> <SettingsIcon /> Settings</Dropdown.Item>
+                <Link to="/" onClick={signOut} class="dropdown-item"> <MeetingRoomIcon /> Logout </Link>
             </DropdownButton>
         </React.Fragment>
     )
