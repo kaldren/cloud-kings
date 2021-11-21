fa0bde428745c58e4db3ac0ba04c6526e2cda74a
Kaloyan Drenski <drenski666@gmail.com>
2021-11-21 22:25:18 +0200

    Added delete pop up for personal items deletion

--- src/components/MyItems.js
+++ src/components/MyItems.js
@@ -6,6 +6,12 @@ import { db } from '../firebase';
 
 import {getUserData} from '../auth';
 import './MyItems.css';
+import GenericButton from './GenericButton';
+
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 function MyItems() {
     const [items, setItems] = useState([]);
@@ -25,16 +31,36 @@ function MyItems() {
         getItems();
     }, [itemsCollectionRef]);
 
+    const submit = () => {
+        confirmAlert({
+          message: 'Are you sure you want to delete this item?',
+          buttons: [
+            {
+              label: 'Yes',
+              onClick: () => alert('Click Yes')
+            },
+            {
+              label: 'No',
+              onClick: () => alert('Click No')
+            }
+          ]
+        });
+      };
+    
+
     return (
         <div id="myItems">
                 {items.map((item) => {
                     return (
-                        <Link 
-                            to={`/items/${item.id}`}
-                            key={item.id}
-                        >
-                            <Item image={item.image} description={item.shortDescription} price={item.price} />
-                        </Link>
+                        <div className='item'>
+                            <Link 
+                                to={`/items/${item.id}`}
+                                key={item.id}
+                            >
+                                <Item image={item.image} description={item.shortDescription} price={item.price} />
+                            </Link>
+                            <GenericButton variant='danger' Icon={DeleteForeverIcon} text='Remove' onClick={submit} />
+                        </div>
                     )
                 })}
         </div>
