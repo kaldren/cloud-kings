0cad649acda12b754a5dd9fef90d90f66f27c8f7
Kaloyan Drenski <drenski666@gmail.com>
2021-11-21 22:13:09 +0200

    List user's own listings

--- src/components/MyItems.js
+++ src/components/MyItems.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import Item from './Item'
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from '../firebase';
+
+import {getUserData} from '../auth';
+import './MyItems.css';
 
 function MyItems() {
+    const [items, setItems] = useState([]);
+
+    const itemsCollectionRef = collection(db, 'items');
+    const user = getUserData();
+
+    useEffect(() => {
+        const getItems = async () => {
+            const itemsQuery = query(itemsCollectionRef, where("email", "==", user.email));
+            
+            var items = await getDocs(itemsQuery);
+
+            setItems(items.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        }
+
+        getItems();
+    }, [itemsCollectionRef]);
+
     return (
-        <div>
-            Here are your items
+        <div id="myItems">
+                {items.map((item) => {
+                    return (
+                        <Link 
+                            to={`/items/${item.id}`}
+                            key={item.id}
+                        >
+                            <Item image={item.image} description={item.shortDescription} price={item.price} />
+                        </Link>
+                    )
+                })}
         </div>
     )
 }
