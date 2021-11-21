74f4e5a3ab9f56ab41b1f58cd40b4a6a848d8fed
Kaloyan Drenski <drenski666@gmail.com>
2021-11-21 11:08:56 +0200

    Added My Items to profile

--- src/components/HeaderAuthenticated.js
+++ src/components/HeaderAuthenticated.js
@@ -11,6 +11,7 @@ import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 import SettingsIcon from '@mui/icons-material/Settings';
 import ForumIcon from '@mui/icons-material/Forum';
 import FiberNewIcon from '@mui/icons-material/FiberNew';
+import ListAltIcon from '@mui/icons-material/ListAlt';
 
 // Custom
 import {signOut} from '../auth'
@@ -22,8 +23,9 @@ function HeaderAuthenticated({user}) {
                 <span className='material-ui-icon'><FiberNewIcon /> </span>
             </Link>
             <DropdownButton id="dropdown-basic-button" title={user.displayName}>
+                <Link to="/my-items" className="dropdown-item"> <ListAltIcon /> My items </Link>
                 <Dropdown.Item href="#/action-2"><ForumIcon /> Messages</Dropdown.Item>
-                <Dropdown.Item href="#/action-1"> <SettingsIcon /> Settings</Dropdown.Item>
+                <Dropdown.Item href="#/action-3"> <SettingsIcon /> Settings</Dropdown.Item>
                 <Link to="/" onClick={signOut} className="dropdown-item"> <MeetingRoomIcon /> Logout </Link>
             </DropdownButton>
         </React.Fragment>
