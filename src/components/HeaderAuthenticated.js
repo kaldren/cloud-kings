a6515f6feb3e159a077ad30ef520fed6f47bcd4e
Kaloyan Drenski <drenski666@gmail.com>
2021-11-21 11:18:19 +0200

    Added profile photo on header

--- src/components/HeaderAuthenticated.js
+++ src/components/HeaderAuthenticated.js
@@ -19,11 +19,10 @@ import {signOut} from '../auth'
 function HeaderAuthenticated({user}) {
     return (
         <React.Fragment>
-            <Link to="/new"> 
-                <span className='material-ui-icon'><FiberNewIcon /> </span>
-            </Link>
+            <img class='profile-photo' src={user.photoURL} alt={user.displayName} />
             <DropdownButton id="dropdown-basic-button" title={user.displayName}>
                 <Link to="/my-items" className="dropdown-item"> <ListAltIcon /> My items </Link>
+                <Link to="/new" className="dropdown-item"> <FiberNewIcon /> Sell item </Link>
                 <Dropdown.Item href="#/action-2"><ForumIcon /> Messages</Dropdown.Item>
                 <Dropdown.Item href="#/action-3"> <SettingsIcon /> Settings</Dropdown.Item>
                 <Link to="/" onClick={signOut} className="dropdown-item"> <MeetingRoomIcon /> Logout </Link>
