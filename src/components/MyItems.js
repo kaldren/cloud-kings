a9b14985630d04611edc37854a83c6240727dd96
Kaloyan Drenski <drenski666@gmail.com>
2021-11-22 22:46:00 +0200

    Fancy deletion of items.

--- src/components/MyItems.js
+++ src/components/MyItems.js
@@ -4,14 +4,19 @@ import Item from './Item'
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from '../firebase';
 import { doc, deleteDoc } from "firebase/firestore";
-import {getUserData} from '../auth';
+import { getUserData } from '../auth';
 import './MyItems.css';
 import GenericButton from './GenericButton';
 
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from '@mui/icons-material/Edit';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+import AlertBox from './AlertBox';
 
 function MyItems() {
     const [items, setItems] = useState([]);
@@ -22,7 +27,7 @@ function MyItems() {
     useEffect(() => {
         const getItems = async () => {
             const itemsQuery = query(itemsCollectionRef, where("email", "==", user.email));
-            
+
             var items = await getDocs(itemsQuery);
 
             setItems(items.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
@@ -31,48 +36,91 @@ function MyItems() {
         getItems();
     }, [itemsCollectionRef]);
 
-    const submit = (e) => {
-        confirmAlert({
-          message: 'Are you sure you want to delete this item?',
-          buttons: [
-            {
-              label: 'Yes',
-              onClick: () => {
-                  const itemId = e.target.parentNode.getAttribute('data-item-id');
+    const [show, setShow] = useState(false);
+    const [selectedItem, setSelectedItem] = useState({});
+    const [isItemDeleted, setIsItemDeleted] = useState(false);
 
-                  deleteDoc(doc(db, "items", itemId))
-                  .then(() => {
-                    console.log('DONE!');
-                  })    
-                  .catch(() => {
-                    console.log('Error!')
-                  });
-              }
-            },
-            {
-              label: 'No',
-              onClick: () => alert('Click No')
-            }
-          ]
-        });
-      };
-    
+    const handleClose = () => setShow(false);
+
+    const handleShowEdit = (e) => {
+        setShow(true);
+        const itemId = e.target.parentNode.parentNode.getAttribute('data-item-id');
+        setSelectedItem(itemId);
+    }
+
+    const handleEdit = (e) => {
+        const itemId = e.target.parentNode.getAttribute('data-item-id');
+
+        deleteDoc(doc(db, "items", itemId))
+            .then(() => {
+                setShow(false);
+            })
+            .catch(() => {
+                console.log('Error!')
+            });
+    }
+
+    const handleShowDelete = (e) => {
+        setShow(true);
+        setIsItemDeleted(false);
+        const itemId = e.target.parentNode.parentNode.getAttribute('data-item-id');
+        const title = e.target.parentNode.parentNode.getAttribute('data-item-description');
+        setSelectedItem({ itemId, title });
+    }
+
+    const handleDelete = () => {
+        deleteDoc(doc(db, "items", selectedItem.itemId))
+            .then(() => {
+                // setShow(false);
+                setIsItemDeleted(true);
+            })
+            .catch(() => {
+                console.log('Error!')
+            });
+    }
+
 
     return (
         <div id="myItems">
-                {items.map((item) => {
-                    return (
-                        <div className='item' data-item-id={item.id} key={item.id}>
-                            <Link 
-                                to={`/items/${item.id}`}
-                                key={item.id}
-                            >
-                                <Item image={item.image} description={item.shortDescription} price={item.price} />
-                            </Link>
-                            <GenericButton variant='danger' Icon={DeleteForeverIcon} text='Remove' onClick={(e) => {submit(e)}} />
+            {items.map((item) => {
+                return (
+                    <div className='item' data-item-id={item.id} key={item.id} data-item-description={item.shortDescription}>
+                        <Link
+                            to={`/items/${item.id}`}
+                            key={item.id}
+                        >
+                            <Item image={item.image} description={item.shortDescription} price={item.price} />
+                        </Link>
+                        <div className='item-options'>
+                            <GenericButton variant='secondary' Icon={EditIcon} text='' onClick={(e) => { handleShowEdit(e) }} />
+                            <GenericButton variant='danger' Icon={DeleteForeverIcon} text='' onClick={(e) => { handleShowDelete(e) }} />
                         </div>
-                    )
-                })}
+                    </div>
+                )
+            })}
+
+            <Modal show={show} onHide={handleClose}>
+                {isItemDeleted === true ? <AlertBox title='Deletion successful.' variant='success' /> :
+                    <div>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Deleting {selectedItem.title}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Are you sure you want to delete this?</Modal.Body>
+                        <Modal.Footer>
+                            <GenericButton
+                                text='Cancel'
+                                variant='secondary'
+                                onClick={handleClose}
+                                classList={isItemDeleted === true ? 'disabled' : ''} />
+                            <GenericButton
+                                text='Delete'
+                                variant='danger'
+                                onClick={handleDelete}
+                                classList={isItemDeleted === true ? 'disabled' : ''} />
+                        </Modal.Footer>
+                    </div>
+                }
+            </Modal>
         </div>
     )
 }
