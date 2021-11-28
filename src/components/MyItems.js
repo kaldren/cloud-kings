957dd3411096a7046f4ea0f6a79d53b9711e15b0
Kaloyan Drenski <drenski666@gmail.com>
2021-11-28 16:16:02 +0200

    Edit form fixes

--- src/components/MyItems.js
+++ src/components/MyItems.js
@@ -135,20 +135,22 @@ function MyItems() {
                         </Modal.Header>
                         <Modal.Body>
                         <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Group className="mb-3" controlId="formShortDescription">
+                                <Form.Label>Short description</Form.Label>
+                                <Form.Control type="text" placeholder="Enter short description" />
                                 <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
+                                Short description about the item you are selling.
                                 </Form.Text>
                             </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                            <Form.Group className="mb-3" controlId="formLongDescription">
+                                <Form.Label>Long description</Form.Label>
+                                <Form.Control as="textarea" placeholder="Long description" />
                             </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Check me out" />
+
+                            <Form.Group className="mb-3" controlId="formPrice">
+                                <Form.Label>Price</Form.Label>
+                                <Form.Control type="number" placeholder="Price" />
                             </Form.Group>
                         </Form>
                         </Modal.Body>
